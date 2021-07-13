abcd20681240b9c098a1ef12e103bd3281a9fea4
wajeht <58354193+wajeht@users.noreply.github.com>
2021-07-13 00:20:07 -0500

    feat: done with client rendering

--- server/controllers/passwordController.js
+++ server/controllers/passwordController.js
@@ -18,9 +18,10 @@ exports.getDefaultPasswordByLength = (req, res) => {
     });
 };
 
-exports.getDefaultPasswordByParam = (req, res) => {
-    const { param } = req.params;
+exports.getPasswordByLengthAndOneParam = (req, res) => {
+    const { length, param } = req.params;
     const password = new Password({
+        length: length,
         [param]: true,
     });
 
@@ -29,11 +30,41 @@ exports.getDefaultPasswordByParam = (req, res) => {
     });
 };
 
-exports.getPasswordByLengthAndParam = (req, res) => {
-    const { length, param } = req.params;
+exports.getPasswordByLengthAndTwoParams = (req, res) => {
+    const { length, one, two } = req.params;
     const password = new Password({
         length: length,
-        [param]: true,
+        [one]: true,
+        [two]: true,
+    });
+
+    res.json({
+        password: password.getPassword(),
+    });
+};
+
+exports.getPasswordByLengthAndThreeParams = (req, res) => {
+    const { length, one, two, three } = req.params;
+    const password = new Password({
+        length: length,
+        [one]: true,
+        [two]: true,
+        [three]: true,
+    });
+
+    res.json({
+        password: password.getPassword(),
+    });
+};
+
+exports.getPasswordByLengthAndFourParams = (req, res) => {
+    const { length, one, two, three, four } = req.params;
+    const password = new Password({
+        length: length,
+        [one]: true,
+        [two]: true,
+        [three]: true,
+        [four]: true,
     });
 
     res.json({
--- server/routes/passwordRouter.js
+++ server/routes/passwordRouter.js
@@ -4,8 +4,10 @@ const router = express.Router();
 const passwordController = require('../controllers/passwordController.js');
 
 router.get('/', passwordController.getDefaultPassword);
-router.get('/:param', passwordController.getDefaultPasswordByParam);
 router.get('/:length', passwordController.getDefaultPasswordByLength);
-router.get('/:length/:param', passwordController.getPasswordByLengthAndParam);
+router.get('/:length/:param', passwordController.getPasswordByLengthAndOneParam);
+router.get('/:length/:one/:two', passwordController.getPasswordByLengthAndTwoParams);
+router.get('/:length/:one/:two/:three', passwordController.getPasswordByLengthAndThreeParams);
+router.get('/:length/:one/:two/:three/:four', passwordController.getPasswordByLengthAndFourParams);
 
 module.exports = router;
