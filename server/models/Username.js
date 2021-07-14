6cdfffa3cb583132c926f80008748ad9a790be0d
wajeht <58354193+wajeht@users.noreply.github.com>
2021-07-14 02:05:35 -0500

    files: add username .json files

--- server/models/Username.js
+++ server/models/Username.js
@@ -1,5 +1,18 @@
+const animals = require('../data/animals.json');
+const nouns = require('../data/nouns.json');
+const adjectives = require('../data/adjectives.json');
+const verbs = require('../data/verbs.json');
+
 class Username {
-    static getRandomAnimal() {}
-    static getRandomAnimal() {}
-    static getRandomAnimal() {}
+    /**
+     * @param {Object}
+     */
+    constructor() {}
+
+    /**
+     * @param {Object}
+     */
+    getUsername() {}
 }
+
+module.exports = Username;
