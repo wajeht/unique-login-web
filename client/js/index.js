d26a345a04e7880c9481c839284d323c27072d95
wajeht <58354193+wajeht@users.noreply.github.com>
2021-07-14 13:05:48 -0500

    feat: username generations

--- client/js/index.js
+++ client/js/index.js
@@ -1,5 +1,7 @@
 // global variable
 const resultBox = document.getElementsByClassName('result')[0];
+const resultBoxPassword = document.getElementsByClassName('result__password')[0];
+const resultBoxUsername = document.getElementsByClassName('result__username')[0];
 const generateButton = document.getElementById('generate');
 const uppercase = document.getElementById('uppercase');
 const lowercase = document.getElementById('lowercase');
@@ -7,7 +9,8 @@ const number = document.getElementById('number');
 const special = document.getElementById('special');
 const passwordLength = document.getElementById('passwordLength');
 
-// to store password options
+// to store password and username options
+let usernameOption = [];
 let passwordOption = [];
 let passwordlengthOption = [];
 
@@ -16,19 +19,19 @@ let passwordlengthOption = [];
  */
 function hideResultBoxOnDomLoad() {
     window.addEventListener('DOMContentLoaded', (event) => {
-        resultBox.style.background = 'white';
-        resultBox.innerHTML = '‏‎ ';
+        resultBox.style.visibility = 'hidden';
+        // resultBox.innerHTML = '‏‎ ';
     });
 }
 
 /**
  * This function will spit out geneated text into result box
- * @param {String} input
+ * @param {String} input, username
  */
-function showResultBox(input) {
-    resultBox.style.background = '#F7F7F7';
-    resultBox.style.color = '#595959';
-    resultBox.innerHTML = input;
+function showResultBox(password, username) {
+    resultBox.style.visibility = 'visible';
+    resultBoxPassword.innerHTML = `<b>Password</b>: ${password}`;
+    resultBoxUsername.innerHTML = `<b>Username</b>: ${username}`;
 }
 
 /**
@@ -56,6 +59,20 @@ function getPasswordCheckboxValue(option) {
 }
 
 /**
+ * This function will get value when user have checked
+ * the username options and push it into an array above
+ * @param {Object} option
+ */
+function getUsernameCheckboxValue(option) {
+    if (option.checked == true) {
+        usernameOption.push(option.value);
+    } else {
+        const pass = usernameOption.filter((pass) => pass != option.value);
+        usernameOption = pass;
+    }
+}
+
+/**
  * This function returns the lenght of an password
  * @returns {Number}
  */
@@ -72,9 +89,37 @@ function getPasswordLengthValue() {
  */
 function getUserInputIntoAPIURL() {
     const length = parseInt(getPasswordLengthValue()) || 14;
-    const params = passwordOption.join('/');
-    // return `${window.location.href}password/${length}/${params}`; // productoin
-    return `http://localhost:6968/password/${length}/${params}`; // development
+    const passwordParams = passwordOption.join('/');
+    const usernameParams = usernameOption.join('/');
+    return {
+        passwordAPI: `${window.location.href}password/${length}/${passwordParams}`,
+        usernameAPI: `${window.location.href}password/${usernameParams}`,
+    };
+    // return {
+    //     passwordAPI: `http://localhost:6968/password/${length}/${passwordParams}`,
+    //     usernameAPI: `http://localhost:6968/username/${usernameParams}`,
+    // };
+}
+
+/**
+ * This function will fetch data and returing passord and username
+ * @returns {Object}
+ */
+async function fetchData() {
+    try {
+        const { passwordAPI, usernameAPI } = getUserInputIntoAPIURL();
+
+        let [password, username] = await Promise.all([
+            fetch(passwordAPI).then((res) => res.json()),
+            fetch(usernameAPI).then((res) => res.json()),
+        ]);
+        return {
+            password: password.password,
+            username: username.username,
+        };
+    } catch (err) {
+        showErrBox(err.message);
+    }
 }
 
 /**
@@ -84,16 +129,10 @@ function getUserInputIntoAPIURL() {
 function generate() {
     // btn 'generate' click
     generateButton.addEventListener('click', () => {
-        const passwordAPI = getUserInputIntoAPIURL();
-        fetch(passwordAPI)
-            .then((res) => res.json())
-            .then((res) => {
-                const { password } = res;
-                showResultBox(password);
-            })
-            .catch((e) => {
-                showErrBox(e.message);
-            });
+        fetchData().then((res) => {
+            const { password, username } = res;
+            showResultBox(password, username);
+        });
     });
 }
 
