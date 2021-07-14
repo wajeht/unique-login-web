76a60004736dc45558cf22a84ef0e2774998cd03
wajeht <58354193+wajeht@users.noreply.github.com>
2021-07-14 03:02:19 -0500

    chore: add username model class

--- server/models/Username.js
+++ server/models/Username.js
@@ -7,12 +7,45 @@ class Username {
     /**
      * @param {Object}
      */
-    constructor() {}
+    constructor(
+        config = {
+            adjectives: true,
+            verbs: true,
+            nouns: true,
+            animals: true,
+        }
+    ) {
+        if (config.adjectives) this.adjectives = JSON.stringify(adjectives);
+        if (config.verbs) this.verbs = JSON.stringify(verbs);
+        if (config.nouns) this.nouns = JSON.stringify(nouns);
+        if (config.animals) this.animals = JSON.stringify(animals);
+    }
 
     /**
      * @param {Object}
      */
-    getUsername() {}
+    getUsername() {
+        let username = '';
+
+        username += 'adjectives' in this ? this.#getRandomElementFromAnArray(this.adjectives) : '';
+        username += 'verbs' in this ? this.#getRandomElementFromAnArray(this.verbs) : '';
+        username += 'nouns' in this ? this.#getRandomElementFromAnArray(this.nouns) : '';
+        username += 'animals' in this ? this.#getRandomElementFromAnArray(this.animals) : '';
+
+        return username;
+    }
+
+    /**
+     * @param {Array}
+     * @returns {String}
+     */
+    #getRandomElementFromAnArray(array) {
+        const res = JSON.parse(array);
+        const min = 0;
+        const max = res.length - 1;
+        const random = Math.floor(Math.random() * (max - min) + min);
+        return res[random];
+    }
 }
 
 module.exports = Username;
