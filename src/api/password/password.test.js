bbc1ae11d6b37eb388ae8c036100f6de69c6639b
wajeht <58354193+wajeht@users.noreply.github.com>
2022-11-27 17:03:03 -0500

    refactor: imporove test cases

--- src/api/password/password.test.js
+++ src/api/password/password.test.js
@@ -17,24 +17,22 @@ describe('GET /api/password', () => {
 
   it('should returns a default password that has some upper case characters', async () => {
     const res = await request(app).get('/api/password');
-    expect(res.body.password.split('').some((c) => c.toUpperCase())).toBe(true);
+    expect(res.body.password.split('').some((c) => /[A-Z]/g.test(c))).toBeTruthy();
   });
 
   it('should returns a default password that has some lower case characters', async () => {
     const res = await request(app).get('/api/password');
-    expect(res.body.password.split('').some((c) => !c.toUpperCase())).toBe(false);
+    expect(res.body.password.split('').some((c) => /[a-z]/g.test(c))).toBeTruthy();
   });
 
   it('should returns a default password that has some number characters', async () => {
     const res = await request(app).get('/api/password');
-    expect(res.body.password.split('').some((c) => '1234567890'.split('').indexOf(c) != -1)).toBe(
-      true,
-    );
+    expect(res.body.password.split('').some((c) => /[0-9]/g.test(c))).toBeTruthy();
   });
 
   it('should returns a default password that has some special characters', async () => {
     const res = await request(app).get('/api/password');
-    expect(res.body.password.split('').some((c) => '!@#$%^&*-.'.split('').includes(c))).toBe(true);
+    expect(res.body.password.split('').some((c) => /[-!$%^&*()_+|~=`{}[:;<>?,.@#\]]/g.test(c))).toBeTruthy(); // prettier-ignore
   });
 });
 
@@ -60,60 +58,60 @@ describe('GET /api/password/32/uppercase', () => {
 
   it('should returns a password that has 32 characters long', async () => {
     const res = await request(app).get('/api/password/32/uppercase');
-    expect(res.body.password.split('').every((c) => c.toUpperCase())).toBe(true);
+    expect(res.body.password.split('').some((c) => /[A-Z]/g.test(c))).toBeTruthy();
   });
 });
 
-describe('GET /api/password/10/lowercase', () => {
-  it('should returns a password wth lowercase that has 10 characters long', async () => {
-    const res = await request(app).get('/api/password/10/uppercase');
-    expect(res.body.password.length).toBe(10);
+describe('GET /api/password/25/lowercase', () => {
+  it('should returns a password wth lowercase that has 25 characters long', async () => {
+    const res = await request(app).get('/api/password/25/uppercase');
+    expect(res.body.password.length).toBe(25);
   });
 
-  it('should returns a password that has all lowercase characters', async () => {
+  it('should returns a password that has all uppercase characters', async () => {
     const res = await request(app).get('/api/password/33/uppercase');
-    expect(res.body.password.split('').every((c) => !c.toUpperCase())).toBe(false);
+    expect(res.body.password.split('').every((c) => /[A-Z]/g.test(c))).toBeTruthy();
   });
 });
 
-describe('GET /api/password/11/number', () => {
-  it('should returns a password wth lowercase that has 11 characters long', async () => {
-    const res = await request(app).get('/api/password/11/number');
-    expect(res.body.password.length).toBe(11);
+describe('GET /api/password/25/number', () => {
+  it('should returns a password wth lowercase that has 25 characters long', async () => {
+    const res = await request(app).get('/api/password/25/number');
+    expect(res.body.password.length).toBe(25);
   });
 
   it('should returns a password that has all number characters', async () => {
     const res = await request(app).get('/api/password/143/number');
-    expect(res.body.password.split('').every((c) => '1234567890'.split('').includes(c))).toBe(true);
+    expect(res.body.password.split('').some((c) => /[0-9]/g.test(c))).toBeTruthy();
   });
 });
 
-describe('GET /api/password/8/number/uppercase', () => {
+describe('GET /api/password/25/number/uppercase', () => {
   it('should returns a password that has some number characters and uppercase', async () => {
-    const res = await request(app).get('/api/password/8/number/uppercase');
-    expect(res.body.password.length).toBe(8);
-    expect(res.body.password.split('').some((c) => '1234567890'.includes(c))).toBe(true);
-    expect(res.body.password.split('').some((c) => c.toUpperCase())).toBe(true);
+    const res = await request(app).get('/api/password/25/number/uppercase');
+    expect(res.body.password.length).toBe(25);
+    expect(res.body.password.split('').some((c) => /[0-9]/g.test(c))).toBeTruthy();
+    expect(res.body.password.split('').some((c) => /[A-Z]/g.test(c))).toBeTruthy();
   });
 });
 
-describe('GET /api/password/10/number/uppercase/lowercase', () => {
+describe('GET /api/password/25/number/uppercase/lowercase', () => {
   it('should returns a password that has some number, uppercase, and lowercase', async () => {
-    const res = await request(app).get('/api/password/10/number/uppercase/lowercase');
-    expect(res.body.password.length).toBe(10);
-    expect(res.body.password.split('').some((c) => '1234567890'.includes(c))).toBe(true);
-    expect(res.body.password.split('').some((c) => c.toUpperCase())).toBe(true);
-    expect(res.body.password.split('').some((c) => !c.toUpperCase())).toBe(false);
+    const res = await request(app).get('/api/password/25/number/uppercase/lowercase');
+    expect(res.body.password.length).toBe(25);
+    expect(res.body.password.split('').some((c) => /[0-9]/g.test(c))).toBeTruthy();
+    expect(res.body.password.split('').some((c) => /[A-Z]/g.test(c))).toBeTruthy();
+    expect(res.body.password.split('').some((c) => /[a-z]/g.test(c))).toBeTruthy();
   });
 });
 
-describe('GET /api/password/10/number/uppercase/lowercase/special', () => {
+describe('GET /api/password/25/number/uppercase/lowercase/special', () => {
   it('should returns a password that has some number, uppercase, lowercase, and special characters', async () => {
-    const res = await request(app).get('/api/password/10/number/uppercase/lowercase/special');
-    expect(res.body.password.length).toBe(10);
-    expect(res.body.password.split('').some((c) => '1234567890'.split('').includes(c))).toBe(true);
-    expect(res.body.password.split('').some((c) => c.toUpperCase())).toBe(true);
-    expect(res.body.password.split('').some((c) => !c.toUpperCase())).toBe(false);
-    expect(res.body.password.split('').some((c) => '!@#$%^&*-.'.includes(c))).toBe(true);
+    const res = await request(app).get('/api/password/25/number/uppercase/lowercase/special');
+    expect(res.body.password.length).toBe(25);
+    expect(res.body.password.split('').some((c) => /[0-9]/g.test(c))).toBeTruthy();
+    expect(res.body.password.split('').some((c) => /[A-Z]/g.test(c))).toBeTruthy();
+    expect(res.body.password.split('').some((c) => /[a-z]/g.test(c))).toBeTruthy();
+    expect(res.body.password.split('').some((c) => /[-!$%^&*()_+|~=`{}[:;<>?,.@#]/g.test(c))).toBeTruthy(); // prettier-ignore
   });
 });
--- src/utils/scratch-pad.js
+++ src/utils/scratch-pad.js
@@ -1,7 +1,10 @@
 // const password = 'PASSWORDpassword!@#$$%%1234';
 
 const password = 'PASSx#@!!';
+const numbers = 'asdf';
 
-const every = password.split('').some((p) => 'b'.split('').includes(p));
+const x = password.split('').some((c) => c.toUpperCase().match('[A-Z]'));
 
-console.log(every);
+const n = numbers.split('').some((c) => c.match('[0-9]'));
+
+console.log(n);
