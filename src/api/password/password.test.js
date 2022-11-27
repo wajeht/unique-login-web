75a3649968b2a943bb11fe93a532cd22280c8ac6
wajeht <58354193+wajeht@users.noreply.github.com>
2022-11-26 22:25:25 -0500

    feat: added test for /api/password/23/lowercase

--- src/api/password/password.test.js
+++ src/api/password/password.test.js
@@ -27,11 +27,49 @@ describe('GET /api/password', () => {
 
   it('should returns a default password that has some number characters', async () => {
     const res = await request(app).get('/api/password');
-    console.log(res.body.password);
-    expect(
-      res.body.password
-        .split('')
-        .some((c) => ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'].indexOf(c)),
-    ).toBe(true);
+    expect(res.body.password.split('').some((c) => '1234567890'.split('').indexOf(c))).toBe(true);
+  });
+
+  it('should returns a default password that has some special characters', async () => {
+    const res = await request(app).get('/api/password');
+    expect(res.body.password.split('').some((c) => '!@#$%^&*-.'.split('').indexOf(c))).toBe(true);
+  });
+});
+
+describe('GET /api/password/20', () => {
+  it('should returns a password that has 20 characters long', async () => {
+    const res = await request(app).get('/api/password/20');
+    expect(res.body.password.length).toBe(20);
+  });
+});
+
+describe('GET /api/password/30', () => {
+  it('should returns a password that has 30 characters long', async () => {
+    const res = await request(app).get('/api/password/30');
+    expect(res.body.password.length).toBe(30);
+  });
+});
+
+describe('GET /api/password/32/uppercase', () => {
+  it('should returns a password that has 32 characters long', async () => {
+    const res = await request(app).get('/api/password/32/uppercase');
+    expect(res.body.password.length).toBe(32);
+  });
+
+  it('should returns a password that has 32 characters long', async () => {
+    const res = await request(app).get('/api/password/32/uppercase');
+    expect(res.body.password.split('').every((c) => c.toUpperCase())).toBe(true);
+  });
+});
+
+describe('GET /api/password/10/lowercase', () => {
+  it('should returns a password wth lowercase that has 10 characters long', async () => {
+    const res = await request(app).get('/api/password/10/uppercase');
+    expect(res.body.password.length).toBe(10);
+  });
+
+  it('should returns a password that has all lowercase characters', async () => {
+    const res = await request(app).get('/api/password/33/uppercase');
+    expect(res.body.password.split('').every((c) => !c.toUpperCase())).toBe(false);
   });
 });
