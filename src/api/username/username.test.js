d2368243eed330c555c7201f5b5a7eb11c76f9a9
wajeht <58354193+wajeht@users.noreply.github.com>
2022-11-27 17:41:53 -0500

    feat: added complete test for /api/username

--- src/api/username/username.test.js
+++ src/api/username/username.test.js
@@ -3,10 +3,55 @@ import { describe, it, expect } from 'vitest';
 import { StatusCodes } from 'http-status-codes';
 
 import app from '../../app.js';
+import { splitStringOnUpperCase } from '../../utils/helpers.js';
+
+import { adjectives as Adjectives } from '../../data/index.js';
+import { animals as Animals } from '../../data/index.js';
+import { verbs as Verbs } from '../../data/index.js';
+import { nouns as Nouns } from '../../data/index.js';
 
 describe('GET /api/username', () => {
   it('should returns a valid status and a default username', async () => {
     const res = await request(app).get('/api/username');
     expect(res.statusCode).toBe(StatusCodes.OK);
+  });
+});
+
+describe('GET /api/username', () => {
+  it('should returns a default username with adjective and animal', async () => {
+    const res = await request(app).get('/api/username');
+    const [adjective, animal] = splitStringOnUpperCase(res.body.username);
+    expect(Animals.includes(animal)).toBeTruthy();
+    expect(Adjectives.includes(adjective)).toBeTruthy();
+  });
+});
+
+describe('GET /api/username/animals/adjectives', () => {
+  it('should returns a username with given adjective and animal', async () => {
+    const res = await request(app).get('/api/username/animals/adjectives');
+    const [adjective, animal] = splitStringOnUpperCase(res.body.username);
+    expect(Animals.includes(animal)).toBeTruthy();
+    expect(Adjectives.includes(adjective)).toBeTruthy();
+  });
+});
+
+describe('GET /api/username/animals/adjectives/verbs', () => {
+  it('should returns a username with given adjective, verb and animal', async () => {
+    const res = await request(app).get('/api/username/adjectives/verbs/animals');
+    const [adjective, verb, animal] = splitStringOnUpperCase(res.body.username);
+    expect(Animals.includes(animal)).toBeTruthy();
+    expect(Adjectives.includes(adjective)).toBeTruthy();
+    expect(Verbs.includes(verb)).toBeTruthy();
+  });
+});
+
+describe('GET /api/username/animals/adjectives/verbs/nouns', () => {
+  it('should returns a username with given adjective, verb, noun, animal', async () => {
+    const res = await request(app).get('/api/username/adjectives/verbs/nouns/animals');
+    const [adjective, verb, noun, animal] = splitStringOnUpperCase(res.body.username);
+    expect(Animals.includes(animal)).toBeTruthy();
+    expect(Adjectives.includes(adjective)).toBeTruthy();
+    expect(Verbs.includes(verb)).toBeTruthy();
+    expect(Nouns.includes(noun)).toBeTruthy();
   });
 });
