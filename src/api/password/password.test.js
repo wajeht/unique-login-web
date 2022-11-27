f691ef8b15069ea4b1fb9a5d845f176bffd16825
wajeht <58354193+wajeht@users.noreply.github.com>
2022-11-26 22:49:05 -0500

    feat: complete tests for /api/password

--- src/api/password/password.test.js
+++ src/api/password/password.test.js
@@ -34,9 +34,9 @@ describe('GET /api/password', () => {
 
   it('should returns a default password that has some special characters', async () => {
     const res = await request(app).get('/api/password');
-    expect(res.body.password.split('').some((c) => '!@#$%^&*-.'.split('').indexOf(c) != -1)).toBe(
-      true,
-    );
+    expect(
+      res.body.password.split('').some((c) => '!@#$%^&*-.'.split('').includes(c) === true),
+    ).toBe(true);
   });
 });
 
@@ -75,5 +75,49 @@ describe('GET /api/password/10/lowercase', () => {
   it('should returns a password that has all lowercase characters', async () => {
     const res = await request(app).get('/api/password/33/uppercase');
     expect(res.body.password.split('').every((c) => !c.toUpperCase())).toBe(false);
+  });
+});
+
+describe('GET /api/password/11/number', () => {
+  it('should returns a password wth lowercase that has 11 characters long', async () => {
+    const res = await request(app).get('/api/password/11/number');
+    expect(res.body.password.length).toBe(11);
+  });
+
+  it('should returns a password that has all number characters', async () => {
+    const res = await request(app).get('/api/password/143/number');
+    expect(res.body.password.split('').every((c) => '1234567890'.split('').includes(c))).toBe(true);
+  });
+});
+
+describe('GET /api/password/8/number/uppercase', () => {
+  it('should returns a password that has some number characters and uppercase', async () => {
+    const res = await request(app).get('/api/password/8/number/uppercase');
+    expect(res.body.password.length).toBe(8);
+    expect(res.body.password.split('').some((c) => '1234567890'.split('').includes(c))).toBe(true);
+    expect(res.body.password.split('').some((c) => c.toUpperCase())).toBe(true);
+  });
+});
+
+describe('GET /api/password/10/number/uppercase/lowercase', () => {
+  it('should returns a password that has some number, uppercase, and lowercase', async () => {
+    const res = await request(app).get('/api/password/10/number/uppercase/lowercase');
+    expect(res.body.password.length).toBe(10);
+    expect(res.body.password.split('').some((c) => '1234567890'.split('').includes(c))).toBe(true);
+    expect(res.body.password.split('').some((c) => c.toUpperCase())).toBe(true);
+    expect(res.body.password.split('').some((c) => !c.toUpperCase())).toBe(false);
+  });
+});
+
+describe('GET /api/password/10/number/uppercase/lowercase/special', () => {
+  it('should returns a password that has some number, uppercase, lowercase, and special characters', async () => {
+    const res = await request(app).get('/api/password/10/number/uppercase/lowercase/special');
+    expect(res.body.password.length).toBe(10);
+    expect(res.body.password.split('').some((c) => '1234567890'.split('').includes(c))).toBe(true);
+    expect(res.body.password.split('').some((c) => c.toUpperCase())).toBe(true);
+    expect(res.body.password.split('').some((c) => !c.toUpperCase())).toBe(false);
+    expect(
+      res.body.password.split('').some((c) => '!@#$%^&*-.'.split('').includes(c) == true),
+    ).toBe(true);
   });
 });
