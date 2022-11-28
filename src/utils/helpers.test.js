584be93f52ec71624be54f267d2312b89c881c19
wajeht <58354193+wajeht@users.noreply.github.com>
2022-11-27 21:55:30 -0500

    feat: added unit test for helpers function

---
+++ src/utils/helpers.test.js
@@ -0,0 +1,11 @@
+import { expect, it, describe } from 'vitest';
+
+import { splitStringOnUpperCase } from './helpers.js';
+
+describe('give a string of pascal case', () => {
+  it('should return array with split items on uppercase words', () => {
+    expect(splitStringOnUpperCase('HelloHowAreYouDoing').sort()).toEqual(
+      ['Hello', 'How', 'Are', 'You', 'Doing'].sort(),
+    );
+  });
+});
